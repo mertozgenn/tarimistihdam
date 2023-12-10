10dcffe90d066da012d370c11a2b1dd09f22e0c5
Ceren Saldıran <cerensaldirann@gmail.com>
2023-12-10 03:05:21 +0300

    created login signin pages

---
+++ WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: /<controller>/
+        [Route("giris-yap")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("kayit-ol")]
+        public IActionResult Signin()
+        {
+            return View();
+        }
+    }
+}
+
